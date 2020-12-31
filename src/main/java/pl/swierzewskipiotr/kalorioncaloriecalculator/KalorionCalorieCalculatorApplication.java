d451d66de6c8eb72b4ff58e5229bbcfa893d55a6
SwierzewskiP <swierzewski.piotr@gmail.com>
2020-12-31 20:36:21 +0100

    Initial commit - Spring Inititializr made project

---
+++ src/main/java/pl/swierzewskipiotr/kalorioncaloriecalculator/KalorionCalorieCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.swierzewskipiotr.kalorioncaloriecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalorionCalorieCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalorionCalorieCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/swierzewskipiotr/kalorioncaloriecalculator/KalorionCalorieCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.swierzewskipiotr.kalorioncaloriecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalorionCalorieCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
